ebb7302d300f5c28e993997bf4b76ed871bd7380
Mark M <medvedevmark1999@gmail.com>
2022-11-17 13:24:08 +0300

    Создание и печать первоночального массива

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+Console.Clear();
+
+int Size()
+{
+Console.WriteLine("Введите длину масива в виде положительного числа: ");
+int length = Convert.ToInt32(Console.ReadLine());
+return length;
+}
+
+void FillArray(string[] collection)
+{
+    for(int i =0; i<collection.Length; i++)
+    {
+        Console.WriteLine("Введите значение строки: ");
+        collection[i]= Convert.ToString(Console.ReadLine());
+    }
+}
+
+void PrintArray(string[] col)
+{
+    for (int position = 0; position < col.Length; position++)
+    {
+        Console.Write($"[{col[position]}] ");
+    }
+    Console.WriteLine();
+}
+
+string[] array = new string [Size()];
+FillArray(array);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
